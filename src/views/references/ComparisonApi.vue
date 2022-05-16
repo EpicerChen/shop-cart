0551daa7af71ee7e07eca845d5777b387b9f7a1f
yongyu.chen <yy.chen@getbrightauto.com>
2022-05-16 13:46:39 +0800

    update compositionApi

--- src/views/references/ComparisonApi.vue
+++ src/views/references/ComparisonApi.vue
@@ -1,18 +1,34 @@
 <template lang="">
     <div>
-        
+        {{count}}
     </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, Ref,reactive } from "vue";
+import { defineComponent, ref, Ref, reactive, onBeforeUnmount, computed } from "vue";
 
 export default defineComponent({
     setup() {
-        const constVar :string ="this is const";
-        let refEXample:Ref<string> = ref("this is ref");
+        const constVar: string = "this is const";
         const obj = reactive({})
-        //TODO computed
+        let refExample: Ref<string> = ref("this is ref");
+
+        onBeforeUnmount(() => {
+
+        })
+
+        const count:Ref<number> = ref(0)
+
         
+        const plusOne = computed({
+            get: () => count.value + 1,
+            set: (newVal) => {
+                count.value = newVal - 1;
+            },
+        });
+
+        //TODO computed
+
+        return { obj, constVar,count }
     }
 })
 </script>
